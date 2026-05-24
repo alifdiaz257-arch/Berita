32770456c63e506cd90c472d9ca1a855c142d7be
Lifx Mode Safe <alif.diaz257@sma.belajar.id>
2026-05-24 17:13:47 +0700

    LiftUpload: upload home.php

---
+++ home.php
@@ -0,0 +1,6 @@
+<?php
+// home.php
+echo "<h1>BREAKING: WEBSITE INI SENGGA DIBIKIN BOLONG!</h1>";
+echo "<img src='https://picsum.photos/800/400' style='width:100%'>";
+echo "<p>Kompasrentan adalah portal khusus uji coba hacking. Semua rentan dari SQLi, XSS, sampai RCE. Selamat belajar jadi hacker, tai!</p>";
+?>
